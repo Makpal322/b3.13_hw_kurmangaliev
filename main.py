e94cc4ccb3007253e592961b8976f6360effe6d8
Makpal322 <64000275+Makpal322@users.noreply.github.com>
2020-06-14 20:31:55 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from HTML_generator import *
+
+
+if __name__ == "__main__":
+      def outputer(output=None):
+        with HTML(output=output) as html_file:
+            with TopLevelTag("head") as head:
+                with Tag("title") as title:
+                    title.text = "hello"
+                    head += title
+            html_file += head
+
+            with TopLevelTag("body") as body:
+                with Tag("h1", klass=("main-text",)) as h1:
+                    h1.text = "Test"
+                    body += h1
+
+                with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
+                    with Tag("p") as paragraph:
+                        paragraph.text = "another test"
+                        div += paragraph
+
+                    with Tag("img", is_single=True, src="/icon.png", data_image="responsive") as img:
+                        div += img
+
+                    body += div
+
+                html_file += body
+
